590774a90f500ce06520810cfdf4e1ada8c5ae98
gruni55 <weigel@lat.rub.de>
2022-06-05 23:15:38 +0200

    some changes in the example for elastic scattering

--- examples/elastic_scattering/elastic1.cpp
+++ examples/elastic_scattering/elastic1.cpp
@@ -10,9 +10,9 @@ int main(int argc, char** argv)
 	Scene S; 
 	S.setnS(1.0);			// set refractive index of surrounding medium to 1.0
 
-	double Rsphere = 50.0;	// Radius of the sphere in µm
+	double Rsphere = 10.0;	// Radius of the sphere in µm
 
-    LightSrcPlane LS (-300.0*ez, 10000, 1.0, 2.0*Rsphere,I*ey,LIGHTSRC_RAYTYPE_RAY);  // Setup of the incident plane wave
+    LightSrcPlane LS (-300.0*ez, 3000, 1.0, 2.0*Rsphere,I*ey,LIGHTSRC_RAYTYPE_RAY);  // Setup of the incident plane wave
 	LS.setk(ez);			// direction of the wave
 	S.addLightSource(&LS);	// add light source to scene
 	
@@ -22,9 +22,9 @@ int main(int argc, char** argv)
 	S.setr0(1000.0);		// set radius of calculating sphere to 1000µm
 	
 	
-	Raytrace_scattering R(S,900,180);	// initialise raytracer for elastic scattering
+	Raytrace_scattering R(S,360,180);	// initialise raytracer for elastic scattering
 	R.setNumReflex(5);		// set number of internal reflections to 5
 	R.trace();				// start raytracing 
-	R.save("elastic1.dat",SAVE_ABS2);	// save results to file (squared absolute value of the electric field |E|²
+	R.save("scatt10.dat",SAVE_ABS2);	// save results to file (squared absolute value of the electric field |E|²
 	return 0;
 }
