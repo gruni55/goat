5461e7e50dffbaf45c25130b9c91c82031f24f72
Thomas Weigel <weigel@lat.rub.de>
2022-12-19 17:07:54 +0100

    shortPulse Class added and prepared for getting refractive index functions

--- raytracing/short_pulses.cpp
+++ raytracing/short_pulses.cpp
@@ -3,6 +3,12 @@ namespace GOAT
 {
 	namespace raytracing
 	{
-
+		shortPulse::shortPulse(int numWvlRanges, wavelengthRange* wvlRanges, Scene *S, std::vector<std::function<std::complex<double>(double) > > nList)
+		{
+			this->numWvlRanges = numWvlRanges;
+			this->wvlRanges = wvlRanges;
+			this->S = S;
+			this->nList = nList;
+		}
 	}
 }
--- raytracing/short_pulses.h
+++ raytracing/short_pulses.h
@@ -1,17 +1,21 @@
 #pragma once
 #include "raytrace.h"
+#include <functional>
+#include <vector>
+
 namespace GOAT
 {
 	namespace raytracing
-	typedef struct
 	{
-		double wvlStart; 
-		double wvlStop; 
-		double dWvl; 
-	} wavelengthRange;
+		struct wavelengthRange
+		{
+			double wvlStart; 
+			double wvlStop; 
+			double dWvl; 
+		};
 
 
-	{
+	
 		/**
 		* @brief Class for the calculation of short pulses 
 		* 
@@ -21,10 +25,12 @@ namespace GOAT
 		class shortPulse
 		{
 		  public:
-			  shortPulse(int numWvlRanges, wavelengthRange* WvlRanges, Scene S);
-			  wavelengthRange* WvlRanges = 0;
-			  int numWvlRanges = 0;
-			  Scene S;
+			  shortPulse(int numWvlRanges, wavelengthRange* WvlRanges, Scene *S, std::vector<std::function<std::complex<double>(double) > > nList);
+
+			  wavelengthRange* wvlRanges = 0; ///< Here, the wavelength ranges for the calculation are stored
+			  int numWvlRanges = 0; ///< number of different wavelength ranges
+			  Scene *S=0; ///< the scene - it contains all information about the light sources, objects etc.
+			  std::vector<std::function<std::complex<double>(double) > > nList; ///< List of the refractive index function for each object
 		};
 	}
 }
