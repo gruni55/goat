1a4dfe1721c5306c955db125a9a4fd99e696fb28
Thomas Weigel <weigel@lat.rub.de>
2023-09-23 16:34:25 +0200

    usp_ellipsoid.cpp (Test program for usp calculations) added

--- examples/usptest/usp_ellipsoid.cpp
+++ examples/usptest/usp_ellipsoid.cpp
@@ -16,7 +16,7 @@ int main (int argc, char **argv)
   coneObj.setActive(false); 
   
 
-  GOAT::maths::Vector<double> DetBoxPos(0,0,100);
+  GOAT::maths::Vector<double> DetBoxPos(0,0,150);
   GOAT::maths::Vector<double> DetBoxDim(30,30,100);
   GOAT::raytracing::Box DetBox(DetBoxPos,DetBoxDim, 1.0);
   DetBox.setActive (true);
@@ -25,16 +25,17 @@ int main (int argc, char **argv)
   
   GOAT::maths::Vector<double> LSPos(0,0,-15);
   double LSD=100.0;
-  int LSnumRays=600000.0;
+  int LSnumRays=50100;
   double LSwvl=1.0;
-  GOAT::raytracing::LightSrcPlane_mc LS(LSPos, LSnumRays, LSwvl, LSD);
+//  GOAT::raytracing::LightSrcPlane LS(LSPos, LSnumRays, LSwvl, LSD);
+GOAT::raytracing::LightSrcRing_mc LS(LSPos,LSnumRays,LSwvl,0,2);
   LS.setk(GOAT::maths::Vector<double> (0.0,0.0,1.0));
   GOAT::raytracing::Scene S;
   
   S.addObject(&coneObj);
   S.addObject(&DetBox);  
   S.addLightSource (&LS);
-  S.setr0 (200.0);
+  S.setr0 (500.0);
   S.setnS(1.0);
   
   std::vector< std::function< std::complex< double >(double) > > nList;
@@ -42,19 +43,21 @@ int main (int argc, char **argv)
   nList.push_back (GOAT::raytracing::n_Vacuum);
   nList.push_back (GOAT::raytracing::n_Vacuum);
 
-  double pulseWidth = 100.0;
-  double refTime = 0.0;
-  double spatialRes = 0.2;
+  double pulseWidth = 100;
+  double refTime = 100.0;
+  double spatialRes = 0.4;
   
   GOAT::raytracing::pulseCalculation pc(S);
   pc.setPulseWidth (pulseWidth);
   pc.setSpatialResolution (spatialRes);
   pc.setRefractiveIndexFunctions(nList);
+  pc.setSpectralRanges (1);
+  pc.setNumWavelengthsPerRange(1);
   
-  double time = 300+135;
+  double time = 400;
   pc.field (time); 
-//   GOAT::raytracing::saveFullE(pc.trafo.SAres,"/home/weigel/data/ellipsoid.dat",0);
-   GOAT::raytracing::saveFullE(pc.trafo.SAres,"/home/weigel/data/test1.dat",1);
+//   GOAT::raytracing::saveFullE(pc.trafo.SAres,"/home/weigel/data/axicon1.dat",0);
+   GOAT::raytracing::saveFullE(pc.trafo.SAres,"/home/weigel/data/test.dat",1);
 
 /*
  GOAT::raytracing::Raytrace_Path rp(S);
